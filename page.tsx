6546651a9a38cb28f1c7da27c1dbb1fb5db22e00
liveankon1 <bindankon@gmail.com>
2026-02-16 09:42:35 -0600

    Delete page.tsx

--- page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ minHeight: "100vh", background: "black", color: "white", padding: 24 }}>
-      <h1>Ankon Portal is LIVE ✅</h1>
-      <p>If you see this, the homepage works.</p>
-    </main>
-  );
-}

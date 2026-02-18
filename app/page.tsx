e7539d3e99ac372d99ab7e548a1b08fe181b7d6f
liveankon1 <bindankon@gmail.com>
2026-02-17 21:46:33 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { HeroSection } from "@/components/home/HeroSection";
+import { LiveCommandPanel } from "@/components/home/LiveCommandPanel";
 import { RecentProjectsSection } from "@/components/home/RecentProjectsSection";
 import { AboutPreviewSection } from "@/components/home/AboutPreviewSection";
 import { ContactPreviewSection } from "@/components/home/ContactPreviewSection";
@@ -7,6 +8,7 @@ export default function HomePage() {
   return (
     <>
       <HeroSection />
+      <LiveCommandPanel />
       <RecentProjectsSection />
       <AboutPreviewSection />
       <ContactPreviewSection />

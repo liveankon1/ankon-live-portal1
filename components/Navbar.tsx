1efdd0408ac4f30f834edf8cd9b6c34cdf084636
liveankon1 <bindankon@gmail.com>
2026-02-19 23:31:32 -0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,7 @@ const navItems = [
   { href: "/", label: "Home" },
   { href: "/downloads", label: "Downloads" },
   { href: "/tools", label: "Tools" },
+  { href: "/reviews", label: "Reviews" },
   { href: "/about", label: "About" },
   { href: "/contact", label: "Contact" }
 ];

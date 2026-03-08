f2ea437c8fd057cd696b54c86fe72cfc4a240387
liveankon1 <bindankon@gmail.com>
2026-03-08 14:53:34 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "@/components/home/HeroSection";
+import { DiscordSinceSection } from "@/components/home/DiscordSinceSection";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
+      <DiscordSinceSection />
     </>
   );
 }
